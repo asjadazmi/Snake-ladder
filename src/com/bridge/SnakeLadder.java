1d722ea76b1040e866dc980f4b46a48b8445b142
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-05-28 18:29:43 +0530

    rolling the dice to get the random value

--- src/com/bridge/SnakeLadder.java
+++ src/com/bridge/SnakeLadder.java
@@ -4,8 +4,9 @@ public class SnakeLadder {
 	public static void main(String args[]) {
 		int playerpostion=0;
 		System.out.println("the player position is:"+playerpostion);
+		double randomnumber = (double) (Math.random() * 6) + 1;
+        int diceRoll =(int)randomnumber;
+        System.out.println("Dice Number =" + " " + diceRoll);
+        
 	}
-	
-	
-
 }
