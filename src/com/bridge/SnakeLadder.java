0dbdcf9970b39d86a93d2a64d210c86c403f30ef
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-05-28 18:18:05 +0530

    Added: the player position is at zero

---
+++ src/com/bridge/SnakeLadder.java
@@ -0,0 +1,11 @@
+package com.bridge;
+
+public class SnakeLadder {
+	public static void main(String args[]) {
+		int playerpostion=0;
+		System.out.println("the player position is:"+playerpostion);
+	}
+	
+	
+
+}
